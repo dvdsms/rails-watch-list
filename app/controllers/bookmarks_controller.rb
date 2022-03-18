b8a7c75229fcad5cba986cd40b929cd12289347a
David Simoes <dvdsms@gmail.com>
2022-03-18 11:35:20 +0100

    rails new

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,12 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: :destroy
-  before_action :set_list, only: [:new, :create]
 
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
@@ -25,13 +25,5 @@ class BookmarksController < ApplicationController
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
-  end
-
-  def set_bookmark
-    @bookmark = Bookmark.find(params[:id])
-  end
-
-  def set_list
-    @list = List.find(params[:list_id])
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,11 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
   end
 
   def show
-    @bookmark = Bookmark.new
-    @review = Review.new(list: @list)
+    @list = List.find(params[:id])
   end
 
   def new
@@ -24,15 +22,12 @@ class ListsController < ApplicationController
   end
 
   def destroy
+    @list = List.find(params[:id])
     @list.destroy
     redirect_to lists_path
   end
 
   private
-
-  def set_list
-    @list = List.find(params[:id])
-  end
 
   def list_params
     params.require(:list).permit(:name)

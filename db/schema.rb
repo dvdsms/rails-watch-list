f94da9852453a669c99622d166b1d6b61daafd89
David Simoes <dvdsms@gmail.com>
2022-03-18 11:29:32 +0100

    god give me strength

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_19_155458) do
+ActiveRecord::Schema.define(version: 2022_03_17_095844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.text "comment"
-    t.bigint "movie_id", null: false
-    t.bigint "list_id", null: false
+    t.string "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "movie_id"
+    t.bigint "list_id"
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
     t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2021_02_19_155458) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.text "overview"
+    t.string "overview"
     t.string "poster_url"
     t.float "rating"
     t.datetime "created_at", precision: 6, null: false
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2021_02_19_155458) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.bigint "list_id", null: false
+    t.integer "list_id", null: false
     t.text "comment"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
